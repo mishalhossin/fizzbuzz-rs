bfb18d3c54606d3170cf434bc336a1fb95a4c082
Mishal <91066601+mishalhossin@users.noreply.github.com>
2024-09-03 23:46:24 +0600

    Initial Commit

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+use std::io;
+
+fn main() {
+    println!("Number for FizzBuzz: ");
+
+    let mut input = String::new();
+
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+
+    let trimmed_input = input.trim();
+
+    match trimmed_input.parse::<i32>() {
+        Ok(num) => {
+            if num % 3 == 0 && num % 5 == 0 {
+                println!("FizzBuzz");
+            } else if num % 3 == 0 {
+                println!("Fizz");
+            } else if num % 5 == 0 {
+                println!("Buzz");
+            } else {
+                println!("{}", num);
+            }
+        }
+        Err(_) => println!("Not a valid number"),
+    }
+}
